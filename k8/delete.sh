a379cb5e86f1639a76f1694a125bbb5f8e0c601e
L4B0MB4 <metalfan65@gmail.com>
2024-11-29 18:07:32 +0100

    add UI deployment and service configuration to Kubernetes

--- k8/delete.sh
+++ k8/delete.sh
@@ -1,5 +1,6 @@
 
 kubectl delete -f ingress.yml 
+kubectl delete -f ui.yml 
 kubectl delete -f identification_query.yml 
 kubectl delete -f identification_command.yml 
 kubectl delete -f evtsrc.yml 
