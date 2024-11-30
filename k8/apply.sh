60ece584a29b5be5a361f64ec22e258c057d4736
L4B0MB4 <metalfan65@gmail.com>
2024-11-30 01:39:20 +0100

    add posting command and query deployments to Kubernetes

--- k8/apply.sh
+++ k8/apply.sh
@@ -2,5 +2,7 @@ kubectl apply -f namespace.yml
 kubectl apply -f evtsrc.yml 
 kubectl apply -f identification_command.yml 
 kubectl apply -f identification_query.yml 
+kubectl apply -f posting_command.yml 
+kubectl apply -f posting_query.yml 
 kubectl apply -f ui.yml 
 kubectl apply -f ingress.yml 
--- k8/delete.sh
+++ k8/delete.sh
@@ -1,6 +1,8 @@
 
 kubectl delete -f ingress.yml 
 kubectl delete -f ui.yml 
+kubectl apply -f posting_query.yml 
+kubectl apply -f posting_command.yml 
 kubectl delete -f identification_query.yml 
 kubectl delete -f identification_command.yml 
 kubectl delete -f evtsrc.yml 
