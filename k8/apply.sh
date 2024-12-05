04f32fdaddb12ced585624ae01f4cd095aff9f34
L4B0MB4 <metalfan65@gmail.com>
2024-12-05 17:48:15 +0100

    add chats command and query deployments, services, and update ingress routing

--- k8/apply.sh
+++ k8/apply.sh
@@ -4,5 +4,7 @@ kubectl apply -f identification_command.yml
 kubectl apply -f identification_query.yml 
 kubectl apply -f posting_command.yml 
 kubectl apply -f posting_query.yml 
+kubectl apply -f chats_command.yml 
+kubectl apply -f chats_query.yml 
 kubectl apply -f ui.yml 
 kubectl apply -f ingress.yml 
--- k8/delete.sh
+++ k8/delete.sh
@@ -1,6 +1,8 @@
 
 kubectl delete -f ingress.yml 
 kubectl delete -f ui.yml 
+kubectl delete -f chats_command.yml 
+kubectl delete -f chats_query.yml 
 kubectl delete -f posting_query.yml 
 kubectl delete -f posting_command.yml 
 kubectl delete -f identification_query.yml 
